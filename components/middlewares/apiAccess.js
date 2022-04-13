50d6df78e3f5c9e5864b928b032c76021533a455
Andrew Antonov <antonov.andrew.jr@gmail.com>
2022-04-13 14:53:53 +0500

    Авторизаия с baerer токеном

--- components/middlewares/apiAccess.js
+++ components/middlewares/apiAccess.js
@@ -3,6 +3,6 @@ const createError = require('http-errors');
 const config = require('../../config.json');
 
 module.exports.isAuth = (req, res, next) => {
-    //if (!req.headers.authorization || req.headers.authorization != config.apiKey) return next(createError(403, 'Не авторизован в API'));
+    if (!req.headers.authorization || req.headers.authorization.split(/\s+/)[1] != config.apiKey) return next(createError(403, 'Не авторизован в API'));
     return next();
 }
